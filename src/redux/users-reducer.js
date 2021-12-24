20d304b6e4aa656ee58dc56d50fde4ce336a883a
Zamdikhanov <oxothuk.sky@mail.ru>
2021-12-24 10:25:14 +0400

    49/100 lesson, redisign user page

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,9 +3,9 @@ const SET_USERS = 'SET-USERS';
 
 let initialState = {
     users: [
-        { id: 1, isfollowed: false, fullName: 'Airat', status: 'junior frontend', location: { city: 'Uyanovsk', country: 'Russia' } },
-        { id: 2, isfollowed: true, fullName: 'Lilya', status: 'I am the best', location: { city: 'Uyanovsk', country: 'Russia' } },
-        { id: 3, isfollowed: false, fullName: 'Bulat', status: 'Hi all', location: { city: 'Uyanovsk', country: 'Russia' } },
+        { id: 1, isfollowed: false, fullName: 'Lenin', foto: 'https://content.tviz.tv/gfx/res/44466/azb4n7wrxnkg0w4gwsk0w0o4o.jpg', status: 'Лежу', location: { city: 'Ulyanovsk', country: 'Russia' } },
+        { id: 2, isfollowed: true, fullName: 'Lilya', foto: 'https://content.tviz.tv/gfx/res/44466/azb4n7wrxnkg0w4gwsk0w0o4o.jpg', status: 'I am the best', location: { city: 'Ulyanovsk', country: 'Russia' } },
+        { id: 3, isfollowed: false, fullName: 'Bulat', foto: 'https://content.tviz.tv/gfx/res/44466/azb4n7wrxnkg0w4gwsk0w0o4o.jpg', status: 'Hi all', location: { city: 'Ulyanovsk', country: 'Russia' } },
     ],
 };
 
@@ -15,7 +15,7 @@ const usersReducer = (state = initialState, action) => {
             return {
                 ...state,
                 users: state.users.map(user => {
-                    if (user.id === action.userId) return { ...user, isfollowed: !user.isfollowed };
+                    if (user.id === action.userId) return {...user, isfollowed: !user.isfollowed };
                     else return user;
                 })
             };
