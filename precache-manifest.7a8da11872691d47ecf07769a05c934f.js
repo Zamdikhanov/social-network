8231e9c83300b518b6432ee8b21f0fb9a5e29bb4
Zamdikhanov <oxothuk.sky@mail.ru>
2021-12-26 22:29:56 +0400

    Updates

---
+++ precache-manifest.7a8da11872691d47ecf07769a05c934f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fece5478e205c302e91",
+    "url": "/social-network/static/css/main.940d8060.chunk.css"
+  },
+  {
+    "revision": "8fece5478e205c302e91",
+    "url": "/social-network/static/js/main.8fece547.chunk.js"
+  },
+  {
+    "revision": "443550cce56efa053a95",
+    "url": "/social-network/static/js/1.443550cc.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "482a23085dbd365360d392037f16e9f5",
+    "url": "/social-network/static/media/unload-avatar.482a2308.webp"
+  },
+  {
+    "revision": "04505faa9ae99916ff085e11cfd142ee",
+    "url": "/social-network/static/media/preloader.04505faa.svg"
+  },
+  {
+    "revision": "7fb9fc7c1c6b5e9d1797ffca65a1640a",
+    "url": "/social-network/index.html"
+  }
+];
